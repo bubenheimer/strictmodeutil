872d2eb6b05ff29920b583ee4113d3779ef912d4
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2019-02-22 07:09:24 -0600

    Flesh out StrictModeCore to support slow calls

--- strictmodecore-debug/src/main/java/org/bubenheimer/android/os/StrictModeCore.java
+++ strictmodecore-debug/src/main/java/org/bubenheimer/android/os/StrictModeCore.java
@@ -10,8 +10,11 @@ import org.bubenheimer.util.Uninstantiable;
 import org.bubenheimer.util.function.Supplier;
 
 public final class StrictModeCore extends Uninstantiable {
-    public static void allowThreadDiskReads(final Runnable runnable) {
-        final StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskReads();
+    private static void allow(
+            final Runnable runnable,
+            final Supplier<StrictMode.ThreadPolicy> policyProcessor
+    ) {
+        final StrictMode.ThreadPolicy oldPolicy = policyProcessor.get();
         try {
             runnable.run();
         } finally {
@@ -19,17 +22,11 @@ public final class StrictModeCore extends Uninstantiable {
         }
     }
 
-    public static void allowThreadDiskWrites(final Runnable runnable) {
-        final StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskWrites();
-        try {
-            runnable.run();
-        } finally {
-            StrictMode.setThreadPolicy(oldPolicy);
-        }
-    }
-
-    public static <T> T allowThreadDiskReads(final Supplier<T> supplier) {
-        final StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskReads();
+    private static <T> T allow(
+            final Supplier<T> supplier,
+            final Supplier<StrictMode.ThreadPolicy> policyProcessor
+    ) {
+        final StrictMode.ThreadPolicy oldPolicy = policyProcessor.get();
         try {
             return supplier.get();
         } finally {
@@ -37,12 +34,48 @@ public final class StrictModeCore extends Uninstantiable {
         }
     }
 
-    public static <T> T allowThreadDiskWrites(final Supplier<T> supplier) {
-        final StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskWrites();
-        try {
-            return supplier.get();
-        } finally {
-            StrictMode.setThreadPolicy(oldPolicy);
-        }
+    public static void allowThreadDiskReads(
+            final Runnable runnable
+    ) {
+        allow(runnable, StrictMode::allowThreadDiskReads);
+    }
+
+    public static void allowThreadDiskWrites(
+            final Runnable runnable
+    ) {
+        allow(runnable, StrictMode::allowThreadDiskWrites);
+    }
+
+    public static <T> T allowThreadDiskReads(
+            final Supplier<T> supplier
+    ) {
+        return allow(supplier, StrictMode::allowThreadDiskReads);
+    }
+
+    public static <T> T allowThreadDiskWrites(
+            final Supplier<T> supplier
+    ) {
+        return allow(supplier, StrictMode::allowThreadDiskWrites);
+    }
+
+    public static void allowSlowCalls(
+            final Runnable runnable
+    ) {
+        allow(runnable, StrictModeCore::allowSlowCalls);
+    }
+
+    public static <T> T allowSlowCalls(
+            final Supplier<T> supplier
+    ) {
+        return allow(supplier, StrictModeCore::allowSlowCalls);
+    }
+
+    private static StrictMode.ThreadPolicy allowSlowCalls() {
+        final StrictMode.ThreadPolicy oldPolicy = StrictMode.getThreadPolicy();
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder(oldPolicy)
+                .permitCustomSlowCalls()
+                .build()
+        );
+        return oldPolicy;
     }
 }
